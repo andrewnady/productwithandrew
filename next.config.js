5fc003bd86a9ac37a6fc457940068be02965dd91
Andrew <andrew@Andrews-MacBook-Pro.local>
2026-04-29 21:08:44 +0300

    Add canonical Link header and trailing slash policy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Link",
+            value:
+              '<https://productwithandrew.com>; rel="canonical"',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
